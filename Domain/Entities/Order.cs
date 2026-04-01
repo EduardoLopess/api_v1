5e903b6165e02ce241f133d220c3fbe06eea1b5a
EduardoLopess <110502271+LeonardoLopes34@users.noreply.github.com>
2026-04-01 03:22:01 -0300

    vo e generic

--- Domain/Entities/Order.cs
+++ Domain/Entities/Order.cs
@@ -5,9 +5,16 @@ namespace api.Domain.Entities
     {
         public int Id { get; set; }
         public required Table Table { get; set; } 
-        public DateTime CreationData { get; set;}
+        public DateTime CreationData { get; private set;}
         public ICollection<Item> Items { get; set; } = [];
         public decimal TotalOrder { get; set;}
-        public string? LockedOrder { get; set; }
+        public bool LockedOrder { get; set; }
+        public string? LockedOrderClientIdentification { get; set;}
+
+        public void SetCrationData ()
+        {
+            CreationData = DateTime.Now;
+        }
+       
     }
 }
